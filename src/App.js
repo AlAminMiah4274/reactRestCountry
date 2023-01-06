bd02abbeac38b66e62dc1e0954eac534f87a32f0
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-06 22:53:03 +0600

    discussed about creating file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Country></Country>
+      <Person></Person>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
-  }, []);
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//   }, []);
 
-  return (
-    <div>
-      <h1>Visiting Every Country of the World !!</h1>
-      <h3>Available Countries: {countries.length}</h3>
-      {
-        countries.map(conutry => <Countries name={conutry.name.common} population={conutry.population}></Countries>)
-      }
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Visiting Every Country of the World !!</h1>
+//       <h3>Available Countries: {countries.length}</h3>
+//       {
+//         countries.map(conutry => <Countries name={conutry.name.common} population={conutry.population}></Countries>)
+//       }
+//     </div>
+//   );
+// }
 
-function Countries(props) {
-  return (
-    <div>
-      <h2>Name: {props.name}</h2>
-      <h4>Population: {props.population}</h4>
-    </div>
-  );
-}
+// function Countries(props) {
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <h4>Population: {props.population}</h4>
+//     </div>
+//   );
+// }
 
 export default App;
