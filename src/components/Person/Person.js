aab02ea28601cef2f36997dea078376e8e728dca
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-07 11:10:33 +0600

    practiced previous lession

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Person = () => {
     return (
         <div>
-            <h1>Person in other file</h1>
+            <h1>Person in another file</h1>
         </div>
     );
 };
